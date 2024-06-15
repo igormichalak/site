9e0580725c9588374e1114b0749209b6d03b7639
Igor Michalak <git@igormichalak.com>
2024-06-15 17:41:55 +0200

    feat: add empty home page

--- cmd/httpserver/handlers.go
+++ cmd/httpserver/handlers.go
@@ -1,9 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/igormichalak/site/view"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("home"))
+	if err := view.Home().Render(r.Context(), w); err != nil {
+		app.error(w, r, err)
+	}
 }
 
 func (app *application) sitemap(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/igormichalak/site
 
 go 1.22.4
+
+require github.com/a-h/templ v0.2.707
